f4f9a1fc66d233e9676500dfff86384f92f2f957
Yiyi Xu <xuyiyi0516@gmail.com>
2023-11-21 23:37:24 -0500

    added devpost button, made light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TextSnippetIcon from "@mui/icons-material/TextSnippet";
+import LogoDevIcon from "@mui/icons-material/LogoDev";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
-export default function App() {
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function App() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+
   return (
-    <div className="centered-container">
-      <Stack direction="row" spacing={1}>
-        <IconButton aria-label="github" onClick={() => {window.open('https://github.com/yiyixuu')}}>
-          <GitHubIcon fontSize='large' />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={`top-right ${theme.palette.mode}`}>
+        <IconButton aria-label="change theme" onClick={colorMode.toggleColorMode}>
+          <DarkModeIcon fontSize="large" />
         </IconButton>
-        <IconButton aria-label="linkedin" onClick={() => {window.open('https://www.linkedin.com/in/yiyi-xuu/')}}>
-          <LinkedInIcon fontSize='large' />
-        </IconButton>
-        <IconButton aria-label="resume">
-          <TextSnippetIcon fontSize='large' />
-        </IconButton>
-      </Stack>
-    </div>
+      </div>
+
+      <div className="centered-container">
+        <Stack direction="row" spacing={1}>
+          <IconButton
+            aria-label="github"
+            onClick={() => {
+              window.open("https://github.com/yiyixuu");
+            }}
+          >
+            <GitHubIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            aria-label="linkedin"
+            onClick={() => {
+              window.open("https://www.linkedin.com/in/yiyi-xuu/");
+            }}
+          >
+            <LinkedInIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            aria-label="devpost"
+            onClick={() => {
+              window.open("https://devpost.com/yiyixu");
+            }}
+          >
+            <LogoDevIcon fontSize="large" />
+          </IconButton>
+          <IconButton aria-label="resume">
+            <TextSnippetIcon fontSize="large" />
+          </IconButton>
+        </Stack>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState('dark');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+        components: {
+          // Add a smooth transition for background-color
+          MuiCssBaseline: {
+            styleOverrides: {
+              body: {
+                transition: 'background-color 0.5s ease-in-out',
+              },
+            },
+          },
+          MuiIconButton: {
+            styleOverrides: {
+              // Add a smooth transition for background-color
+              root: {
+                transition: 'background-color 0.5s ease-in-out',
+              },
+            },
+          },
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
