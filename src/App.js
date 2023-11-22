8b7f7f7e38f197381962877483ea39bcb5924cb5
Yiyi Xu <xuyiyi0516@gmail.com>
2023-11-21 21:21:02 -0500

    made 3 buttons with 2 redirects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-function App() {
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centered-container">
+      <Stack direction="row" spacing={1}>
+        <IconButton aria-label="github" onClick={() => {window.open('https://github.com/yiyixuu')}}>
+          <GitHubIcon fontSize='large' />
+        </IconButton>
+        <IconButton aria-label="linkedin" onClick={() => {window.open('https://www.linkedin.com/in/yiyi-xuu/')}}>
+          <LinkedInIcon fontSize='large' />
+        </IconButton>
+        <IconButton aria-label="resume">
+          <TextSnippetIcon fontSize='large' />
+        </IconButton>
+      </Stack>
     </div>
   );
 }
-
-export default App;
